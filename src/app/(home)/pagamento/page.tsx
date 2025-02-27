66c127b72f997237cff1d06043c25cff42f0170a
BernardoRigolon <berigolon@gmail.com>
2025-02-27 00:10:41 -0300

    página de pagamento

--- src/app/(home)/pagamento/page.tsx
+++ src/app/(home)/pagamento/page.tsx
@@ -1,24 +1,47 @@
+'use client'
+
 import getCard from "@/actions/home/actions";
 import { fetchFilteredPosts } from "@/actions/search/actions";
+import CartCard from "@/components/cart/card";
 import Payment from "@/components/payment";
+import { CartContext, CartItem } from "@/context/cart";
+import Link from "next/link";
+import { useContext } from "react";
 
-export default async function Pagamento({searchParams,
-}: {
-    searchParams: {
-        query?: string;
-        page?: string;
+export default function Pagamento() {
+
+    const cartContext = useContext(CartContext);
+
+    if (!cartContext) {
+        throw new Error("CartContext is not provided!");
     }
-}) {
 
-    const query = searchParams?.query || ''
-    const currentPage = Number(searchParams?.page) || 1
+    const { count, cartItems } = cartContext;
 
-    const {posetaxs, count, totalPages} = await fetchFilteredPosts(query, currentPage || 6)
+    const cardItemCount = count();
 
-    const posts = await getCard()
+    const totalValue = cartItems.reduce((acc, item) => acc + item.product?.price, 0);
+
+    const handleClick = () => {
+        alert('Compra realizada com sucesso!');
+      };
+
     return(
         <div className="bg-gradient-to-b from-black to-[#038C00] w-full min-h-screen">
-            <Payment posetaxs={posetaxs} count={count} totalPages={totalPages} />
+            <div className="flex flex-col items-center justify-center py-6 gap-5 lg:gap-10 pb-10">
+            <div className="flex flex-wrap gap-5 lg:gap-20 justify-center items-center">
+                {cartItems.map((item: CartItem, index: number)=>(
+                                    <div key={index}>
+                                        <CartCard post={item.product} />
+                                    </div>
+                                ))}
+            </div>
+            <h2 className="text-white text-2xl lg:text-5xl">Valor Total: R${totalValue.toFixed(2)}</h2>
+            <h3 className="text-white text-xl lg:text-4xl">(24)987654321</h3>
+            <Link href={'/'}>
+            <button onClick={handleClick} className="bg-[#D5D918] p-1 text-xl lg:text-4xl lg:p-2 rounded-sm">Confirmar Compra</button>
+            </Link>
+        </div>
         </div>
     )
 }
